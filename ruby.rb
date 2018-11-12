1d70c6d6ca2098187bbc9f2fcf5a90d8094aad39
sarpong <abasimisarpong@rocketmail.com>
2018-11-12 16:23:50 +0000

    Added a class method to square class

--- ruby.rb
+++ ruby.rb
@@ -1,10 +1,17 @@
+#Shape is the parent class
 class Shape
 end
 
+#Square inherits from the parents class
 class Square < Shape
+    #class variable to track the number of instances of class created
+    @@Square_created=0
+    
     attr_reader :area
     def initialize(side_length)
         @side_length=side_length
+        @@Square_created += 1
+
     end
 
     def area
@@ -14,8 +21,13 @@ class Square < Shape
     def paramiter
         @side_length * 4
     end
+    #class method for the class Square
+    def self.num_squares
+        return @@Square_created
+    end
 end
 
 
 firstSquare = Square.new(5)
-puts firstSquare.area
+myCount = Square.new(75)
+puts Square.num_squares
