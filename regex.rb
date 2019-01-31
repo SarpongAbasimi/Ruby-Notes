56f2807a36c6e8f21ba1b4f0e7cebd49d7fed6d3
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-01-31 13:09:56 +0000

    Update regex.rb

--- regex.rb
+++ regex.rb
@@ -7,4 +7,9 @@ In ruby patterns are matched inside the two //.
 eg) 'Hello Boy'.match(/B/)  #This is saying is there a match  for letter'B' in the  'Hello Boy' string object
 Another means of matching items in ruby is using the =~ sign.This returns the position of the matched character.
 eg) 'The tree' =~ /t/  #this will return 4 since t is at possition 4
+Some commonly used regex.
+1) The dot wildcard(.) eg /.ele/ #This is saying match any letter followed by ele.
+2)Character classses eg /[dr]ae/ #This is saying , match characters starting with eaither a d or r followed by ae
 =end
+
+
