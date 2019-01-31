65acb614711c690540959f0784e6f81242f7b0f3
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-01-31 11:49:01 +0000

    Create regex.rb

---
+++ regex.rb
@@ -0,0 +1,10 @@
+#Regular expression in Ruby
+
+=begin
+Regular expressions are used to match patterns.
+In ruby patterns are matched inside the two //.
+
+eg) 'Hello Boy'.match(/B/)  #This is saying is there a match  for letter'B' in the  'Hello Boy' string object
+Another means of matching items in ruby is using the =~ sign.This returns the position of the matched character.
+eg) 'The tree' =~ /t/  #this will return 4 since t is at possition 4
+=end
